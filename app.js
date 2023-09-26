4ccbcae621aaf7982019a8f14ba8a8265e95af9c
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2023-09-26 16:24:02 -0500

    final changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-answers = ["rock", "paper", "scissors"];
+const answers = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 
@@ -7,57 +7,38 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-  let question = prompt("Select rock, paper or scissors");
-  return question;
+  let question = prompt("Select rock, paper, or scissors");
+  return question.toLowerCase();
 }
 
 function playGame() {
-  while (playerScore < 3 || computerScore < 3) {
-    playRound();
+  while (playerScore < 3 && computerScore < 3) {
+    console.log(playRound());
+  }
+
+  if (playerScore > computerScore) {
+    return "Player wins the game!";
+  } else {
+    return "Computer wins the game!";
   }
 }
 
 function playRound() {
-  const computerChoice = getComputerChoice();
-  const playerChoice = getPlayerChoice().toLowerCase();
+  let computerChoice = getComputerChoice();
+  let playerChoice = getPlayerChoice();
 
   if (playerChoice === computerChoice) {
-    return `You both picked ${playerChoice}, draw. The score is player: ${playerScore} computer: ${computerScore}`;
+    return `You both picked ${playerChoice}, it's a draw. The score is player: ${playerScore} computer: ${computerScore}`;
+  } else if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "paper" && computerChoice === "rock") ||
+    (playerChoice === "scissors" && computerChoice === "paper")
+  ) {
+    playerScore++;
+    return `You win! ${playerChoice} beats ${computerChoice}. The score is player: ${playerScore} computer: ${computerScore}`;
   } else {
-    if (playerChoice === "rock") {
-      switch (computerChoice) {
-        case "paper":
-          computerScore++;
-          return `Paper beats rock, player loses. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-        case "scissors":
-          playerScore++;
-          return `Rock beats scissors, player wins. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-      }
-    } else if (playerChoice === "paper") {
-      switch (computerChoice) {
-        case "rock":
-          playerScore++;
-          return `Paper beats rock, player wins. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-        case "scissors":
-          computerScore++;
-          return `Scissors beats paper, player loses. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-      }
-    } else {
-      switch (computerChoice) {
-        case "rock":
-          computerScore++;
-          return `Rock beats scissors, player loses. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-        case "paper":
-          playerScore++;
-          return `Scissors beats paper, player wins. The score is player: ${playerScore} computer: ${computerScore}`;
-          break;
-      }
-    }
+    computerScore++;
+    return `Computer wins! ${computerChoice} beats ${playerChoice}. The score is player: ${playerScore} computer: ${computerScore}`;
   }
 }
 
